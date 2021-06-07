24b05af8c45836068f1a1f272db1e0379a4533d1
Bukii <lukas.weingand@gmail.com>
2021-06-07 16:15:52 +0200

    We've known each other for so long

--- src/main/java/at/deppn/H2toGraph/types/ChartPointSeries.java
+++ src/main/java/at/deppn/H2toGraph/types/ChartPointSeries.java
@@ -1,7 +1,5 @@
 package at.deppn.H2toGraph.types;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,17 +12,7 @@ public class ChartPointSeries {
 		this.name = name;
 		
 		double[] yData = new double[hs.size() + 1];
-		if (name.equals("Mika")) {
-			yData[0] = (calcTotalPoints(chartPoints)
-					/ (ChronoUnit.DAYS.between(LocalDate.of(2020, 12, 25), LocalDate.now()) - DaysOff.MIKA_DAYS_OFF))
-					* DaysOff.MIKA_DAYS_OFF;
-		} else if (name.equals("Luki")) {
-			yData[0] = (calcTotalPoints(chartPoints)
-					/ (ChronoUnit.DAYS.between(LocalDate.of(2020, 12, 25), LocalDate.now()) - DaysOff.LUKI_DAYS_OFF))
-					* DaysOff.LUKI_DAYS_OFF;
-		} else {
-			yData[0] = 0;
-		}
+		yData[0] = 0;
 		double before = yData[0];
 		for (int i = 1; i < yData.length; i++) {
 			boolean set = false;
@@ -42,16 +30,6 @@ public class ChartPointSeries {
 		}
 
 		this.yData = yData;
-	}
-
-	public double calcTotalPoints(List<ChartPoint> chartPoints) {
-		double total = 0;
-		for (ChartPoint cp : chartPoints) {
-			if (cp.getName().equals(this.name)) {
-				total += cp.getPoints();
-			}
-		}
-		return total;
 	}
 
 	public String getName() {
